6f4e463fe20eed1abce59612531a6706d98eace3
rozsabence <robence@gmail.com>
2019-07-06 13:24:09 +0200

    add eslint dependency installer scripts

---
+++ .install-eslint-and-prettier-yarn.sh
@@ -0,0 +1,13 @@
+yarn add --dev \
+    # These are commented out due to
+    # being installed by create-react-app
+    # Uncomment them if you need them
+    # eslint \
+    # babel-eslint \
+    prettier \
+    eslint-config-airbnb \
+    eslint-config-prettier \
+    eslint-plugin-prettier \
+    eslint-plugin-import \
+    eslint-plugin-jsx-a11y \
+    eslint-plugin-react
