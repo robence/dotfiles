ee51500243cfed1aba7137660c51007af2fa1b37
rozsabence <robence@gmail.com>
2019-07-07 15:04:13 +0200

    add eslint typescript dependency install script

---
+++ .install-eslint-and-prettier-typescript-npm.sh
@@ -0,0 +1,9 @@
+npm install --save-dev \
+    eslint \
+    prettier \
+    @typescript-eslint/parser \
+    @typescript-eslint/eslint-plugin \
+    eslint-config-airbnb-base \
+    eslint-config-prettier \
+    eslint-plugin-prettier \
+    eslint-plugin-import
