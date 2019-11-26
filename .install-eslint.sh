e0786429d0b1d699c2c170b841cb7ee7f29b76df
rozsabence <robence@gmail.com>
2019-11-26 12:22:53 +0100

    update configs

--- .install-eslint.sh
+++ .install-eslint.sh
@@ -1,10 +1,15 @@
 yarn add --dev \
-    eslint \
-    babel-eslint \
+    eslint-config-react-app \
+    @typescript-eslint/eslint-plugin@2.x \
+    @typescript-eslint/parser@2.x \
+    babel-eslint@10.x \
+    eslint@6.x \
+    eslint-plugin-flowtype@3.x \
+    eslint-plugin-import@2.x \
+    eslint-plugin-jsx-a11y@6.x \
+    eslint-plugin-react@7.x \
+    eslint-plugin-react-hooks@1.x \
     prettier \
     eslint-config-prettier \
     eslint-plugin-prettier \
-    eslint-plugin-import \
-    eslint-plugin-jsx-a11y \
-    eslint-plugin-react \
     eslint-config-airbnb
--- .update_remotes.sh
+++ .update_remotes.sh
@@ -1,4 +1,4 @@
-cp ~/.hyper.js ~/Github/dotfiles
 cp ~/.bash_aliases ~/Github/dotfiles
 cp ~/.zshrc ~/Github/dotfiles
+cp ~/.vimrc ~/Github/dotfiles
 cp ~/Library/Application\ Support/Code/User/settings.json ~/Github/dotfiles/.vscode/
