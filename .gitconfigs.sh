5178763a3c30ecd0ae5a09f2f8ce3b2f8f918095
Bence Rozsa <robence@gmail.com>
2020-11-05 14:03:44 +0100

    fix gitconfigs

--- .gitconfigs.sh
+++ .gitconfigs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git config --global user.name "Bence Rozsa"
-git config --global user.email "robence@gmil.com"
+git config --global user.email "robence@gmail.com"
 git config --global core.editor vim
 
 git config --global alias.co checkout
@@ -9,7 +9,7 @@ git config --global alias.c commit
 git config --global alias.st status
 git config --global alias.br branch
 git config --global alias.pull pull --rebase
-git config --global alias.lg "log --graph --pretty=format:'%C(auto)%h -%d %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.hist "log --graph --pretty=format:'%C(auto)%h -%d %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
 git config --global pull.rebase true
 git config --global rebase.autoStash true
